afc0ac40dc7c2b4b99ee077a6124b000e48b703c
Your Name <you@example.com>
2025-07-31 20:40:21 -0300

    pagCursos all

--- src/app/cursos/software/page.tsx
+++ src/app/cursos/software/page.tsx
@@ -3,39 +3,41 @@ import Link from 'next/link';
 
 export default function Software() {
   return (
-    <div className="min-h-screen w-full max-w-5xl mx-auto p-6 bg-gray-800 text-white">
-      <h1 className="text-3xl font-bold mb-6 text-center">Bem vindo usuário, qual sua duvida ?</h1>
+    <div className="flex justify-center min-h-screen w-full mx-auto p-6 bg-gray-800 text-white">
+      <div className='max-w-5xl'>
+        <h1 className="text-3xl font-bold mb-6 text-center">Bem vindo USUÁRIO! qual sua duvida ?</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        {/* Link 1 */}
-        <Link href="/cursos/cadastro-unidade" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
-          Cadastro de Unidade
-        </Link>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          {/* Link 1 */}
+          <Link href="/cursos/cadastro-unidade" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
+            Cadastro de Unidade
+          </Link>
 
-        {/* Link 2 */}
-        <Link href="/cursos/cadastro-morador" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
-          Cadastro de Morador
-        </Link>
+          {/* Link 2 */}
+          <Link href="/cursos/cadastro-morador" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
+            Cadastro de Morador
+          </Link>
 
-        {/* Link 3 */}
-        <Link href="/cursos/ativando-app" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
-          Ativando Aplicativo
-        </Link>
+          {/* Link 3 */}
+          <Link href="/cursos/ativando-app" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
+            Ativando Aplicativo
+          </Link>
 
-        {/* Link 4 */}
-        <Link href="/cursos/ativar-remover-facial" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
-          Ativando e Removendo Facial
-        </Link>
+          {/* Link 4 */}
+          <Link href="/cursos/ativar-remover-facial" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
+            Ativando e Removendo Facial
+          </Link>
 
-        {/* Link 5 */}
-        <Link href="/cursos/cadastro-veiculo" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
-          Cadastro de Veículo
-        </Link>
+          {/* Link 5 */}
+          <Link href="/cursos/cadastro-veiculo" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
+            Cadastro de Veículo
+          </Link>
 
-        {/* Link 6 */}
-        <Link href="/cursos/ativando-tag-veiculo" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
-          Ativando Tag do Veículo
-        </Link>
+          {/* Link 6 */}
+          <Link href="/cursos/ativando-tag-veiculo" className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
+            Ativando Tag do Veículo
+          </Link>
+        </div>
       </div>
     </div>
   );
