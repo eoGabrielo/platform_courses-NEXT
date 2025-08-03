1b9bb8739baa8fd636897f752678ae89c88cf3b7
Your Name <you@example.com>
2025-08-03 19:44:59 -0300

    layout all color2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -50,7 +50,7 @@ export default function Header() {
         <nav
           className={`
             ${menuOpen ? "flex" : "hidden"}
-            absolute top-16 left-0 w-full bg-gray-800 bg-opacity-95 z-20 flex-col items-center gap-4 py-4
+            absolute top-29 left-0 w-full bg-gray-800 bg-opacity-95 z-20 flex-col items-center gap-4 py-4
             sm:static sm:flex sm:flex-row sm:items-center sm:gap-8 sm:bg-transparent sm:py-0 sm:w-auto
             transition-all
           `}
