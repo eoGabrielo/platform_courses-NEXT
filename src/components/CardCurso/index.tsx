81688c4b8770a57be54693b6acf07b47e9d6f81e
Your Name <you@example.com>
2025-08-06 23:17:54 -0300

    cadastro hikivision2

--- src/components/CardCurso/index.tsx
+++ src/components/CardCurso/index.tsx
@@ -1,16 +1,18 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-
-
-export default function CardCurso ({assunto, link}: {assunto: string, link: string} ){
-    return(
-        <Link href={link} className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition">
-            {assunto}
-          </Link>
-    )
+// Definindo os tipos com interface
+interface CardCursoProps {
+  assunto: string;
+  link: string;
 }
 
-
-
-
-
+export default function CardCurso({ assunto, link }: CardCursoProps) {
+  return (
+    <Link
+      href={link}
+      className="block bg-gray-900 hover:bg-gray-600 rounded p-4 text-lg font-semibold text-center transition"
+    >
+      {assunto}
+    </Link>
+  );
+}
